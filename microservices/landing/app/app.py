f8e01346f7e8c1647c6040cb1c5ca5adcae92938
Uthpal-p <77720989+Uthpal-p@users.noreply.github.com>
2022-03-16 17:58:42 +0530

    task 1.1

--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -21,18 +21,19 @@ def divide(n1, n2):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    number_1 = float(request.form.get("first"))
-    number_2 = float(request.form.get('second'))
-    operation = request.form.get('operation')
-    result = 0
-    if operation == 'add':
-        result = add(number_1, number_2)
-    elif operation == 'minus':
-        result =  minus(number_1, number_2)
-    elif operation == 'multiply':
-        result = multiply(number_1, number_2)
-    elif operation == 'divide':
-        result = divide(number_1, number_2)
+    if(request.form!={}):
+        number_1 = float(request.form.get("first"))
+        number_2 = float(request.form.get('second'))
+        operation = request.form.get('operation')
+        result = 0
+        if operation == 'add':
+            result = add(number_1, number_2)
+        elif operation == 'minus':
+            result =  minus(number_1, number_2)
+        elif operation == 'multiply':
+            result = multiply(number_1, number_2)
+        elif operation == 'divide':
+            result = divide(number_1, number_2)
 
     flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
