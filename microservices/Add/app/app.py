0a83e75104d5037c54a23bf02992662eb885878d
JGVishnu <64395028+VishnuJG@users.noreply.github.com>
2022-03-21 10:07:59 +0530

    task 2.0 add service
    Topper's commitq

---
+++ microservices/Add/app/app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask
+from flask_restful import Api, Resource
+
+import requests
+import os
+
+app = Flask(__name__)
+api = Api(app)
+app.secret_key = 'thisisjustarandomstring'
+
+
+class Addition(Resource):
+        
+    def get(self,num1,num2):
+        return {"ans":num1+num2}
+    
+api.add_resource(Addition, "/<int:num1>/<int:num2>")
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=5051,
+        host="add-service"
+    )
+
+
