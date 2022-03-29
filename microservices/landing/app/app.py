d442ce649b8c5381651e6202d2b33c9ed13339c6
JGVishnu <64395028+VishnuJG@users.noreply.github.com>
2022-03-29 11:46:27 +0530

    Task 2.1
    Topper's Submission

--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -11,8 +11,15 @@ app.secret_key = 'thisisjustarandomstring'
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if(request.form!={}):
-        number_1 = int(request.form.get("first"))
-        number_2 = int(request.form.get('second'))
+         
+        number_1 = request.form.get("first")
+        number_2 = request.form.get('second')
+        if number_1.isnumeric() and number_2.isnumeric():
+            number_1=int(number_1)
+            number_2=int(number_2)
+        else:
+            flash(f'Enter valid Input...')
+            return render_template('index.html')
         operation = request.form.get('operation')
         result = 0
         if operation == 'add':
@@ -26,7 +33,17 @@ def index():
             
         elif operation == 'divide':
             res = requests.get("http://div-service:5054/{}/{}".format(number_1,number_2)).json()
-
+        
+        elif operation == 'equal':
+            res = requests.get("http://equal-service:5057/{}/{}".format(number_1,number_2)).json()
+            
+        elif operation == 'exponent':
+            res = requests.get("http://exponent-service:5056/{}/{}".format(number_1,number_2)).json()
+            
+        elif operation == 'modulus':
+            res = requests.get("http://modulus-service:5055/{}/{}".format(number_1,number_2)).json()
+            
+        
         result = res["ans"]
         flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
